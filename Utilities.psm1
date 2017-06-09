0aa5dd1c8d3d4b11da13cfc4b4e620a48d91f1b8
Accalia <Accalia.de.elementia@gmail.com>
2017-06-09 12:57:13 -0400

    add Random Data Generating Utilities

---
+++ Utilities.psm1
@@ -0,0 +1,55 @@
+﻿$Assembly = Add-Type -AssemblyName System.Web
+
+Function New-RandomBinaryFile {
+    Param(
+        $FileSize = 4kb,
+        $Path = (Resolve-Path '.').Path,
+        $FileName = [guid]::NewGuid().Guid + '.dat',
+        $ChunkSize = 4kb
+    )
+    $chunks = [math]::Ceiling($FileSize / $ChunkSize)
+    [io.FileStream]$file = $null
+    try {
+        $file = [io.file]::OpenWrite($Path + '\' + $FileName)
+        $random = New-Object System.Random
+        for ($i = 0; $i -lt $chunks; $i += 1) {
+            [Byte[]] $bytes = New-Object byte[] $ChunkSize
+            $random.NextBytes($bytes)
+            $file.Write($bytes,0,$ChunkSize)
+        }
+    } finally {
+        if ($null -ne $file) {
+            $file.Dispose();
+        }
+    }
+    $di = Get-ChildItem $path -Filter $FileName
+    return $di
+}
+
+Function New-RandomTextFile {
+    Param(
+        $FileSize = 4kb,
+        $Path = (Resolve-Path '.').Path,
+        $FileName = [guid]::NewGuid().Guid + '.dat',
+        $ChunkSize = 128
+    )
+    $enc = [System.Text.Encoding]::UTF8
+    if ($ChunkSize -gt 128) {
+        $ChunkSize = 128
+    }
+    $chunks = [math]::Ceiling($FileSize / $ChunkSize)
+    [io.FileStream]$file = $null
+    try {
+        $file = [io.file]::OpenWrite($Path + '\' + $FileName)
+        for ($i = 0; $i -lt $chunks; $i += 1) {
+            $chunk = [System.Web.Security.Membership]::GeneratePassword($ChunkSize, 0)
+            $file.Write($enc.GetBytes($chunk),0,$ChunkSize)
+        }
+    } finally {
+        if ($null -ne $file) {
+            $file.Dispose();
+        }
+    }
+    $di = Get-ChildItem $path -Filter $FileName
+    return $di
+}
