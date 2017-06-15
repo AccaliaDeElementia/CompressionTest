525197a6119a91e4fee7b1455ef714a7ef8a6b3e
accalia <accalia.de.elementia@gmail.com>
2017-06-15 13:43:09 -0400

    ensure destination folder is created

--- Test.ps1
+++ Test.ps1
@@ -5,6 +5,7 @@ Import-Module -Force './Analysis.psm1'
 $Source = Get-Item -Path '.\Corpus\King James.txt'
 $Dest = Compress_7z $Source
 $Result = $Dest | Where-Object {$_ -ne $null} | %{Analyze_Result $_ $False}
+$ignored = New-Item -Type Directory Results -Force
 Write-Host "Analyzing Results..."
 Write_ResultsCSV $Result
 Write-Host "Done"
