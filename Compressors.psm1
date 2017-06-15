fd42054093915bccc680a71dbfc44c42ccc3b9c7
Accalia <Accalia.de.elementia@gmail.com>
2017-06-15 12:30:43 -0400

    more compression methods

--- Compressors.psm1
+++ Compressors.psm1
@@ -1,11 +1,22 @@
-﻿
+﻿Add-Type -TypeDefinition @"
+   public enum CompressionPreset
+   {
+      Fastest=1,
+      Fast=3,
+      Normal=5,
+      Maximum=7,
+      Ultra=9
+   }
+"@
+
 Function Calculate-Size {
     Param(
         $Target
     )
     if ($Target.PSIsContainer -eq $false) {
-        return $Input.Length
+        return $Target.Length
     }
+    
     $Size = 0
     foreach ($Item in Get-ChildItem -Recurse -Path $Target.FullName | Where-Object {$_.PSIsContainer -eq $false}) {
         $Size += $Item.Length
@@ -39,22 +50,23 @@ Function Format-Result {
 Function Compress-7z {
     Param(
         $Target,
-        [string]$FileName = [guid]::NewGuid().Guid + '.7z',
         [string]$Type = '7z',
         [string]$Mode = 'LZMA',
         [int]$Level = 9,
         [string]$Label = 'Generic 7Zip Test'
     )
+    $FileName = [guid]::NewGuid().Guid + '.' + $Type.ToLower()
+
     $Path = (Resolve-Path '.').Path
     $Dest = $($Path + '\' + $FileName)
 
     $ArchiveType = '-t' + $Type
-    $ArchiveMode = '-m0=' + $Mode
+    $ArchiveMode = '-mm=' + $Mode
     $CompressionLevel = '-mx=' + $Level
     
     $Arguments = "$ArchiveType $ArchiveMode $CompressionLevel"
     
-    $stdout = 7z a $ArchiveType $ArchiveMode $CompressionLevel $Dest $Target.FullName
+    $stdout = 7z a $Dest $ArchiveType $ArchiveMode $CompressionLevel $Target.FullName
     $archive = Get-Item $Dest
 
     return Format-Result $Target $archive $Label '7z' $Arguments
@@ -64,62 +76,30 @@ Function Compress_7z {
     Param(
         $Target
     )
-    $Results = @(
-        (Compress-7z $Target -Label '7z LZMA Fastest Preset' -Mode 'LZMA' -Level 1),
-        (Compress-7z $Target -Label '7z LZMA Fast Preset' -Mode 'LZMA' -Level 3),
-        (Compress-7z $Target -Label '7z LZMA Normal Preset' -Mode 'LZMA' -Level 5),
-        (Compress-7z $Target -Label '7z LZMA Maximum Preset' -Mode 'LZMA' -Level 7),
-        (Compress-7z $Target -Label '7z LZMA Ultra Preset' -Mode 'LZMA' -Level 9),
-        (Compress-7z $Target -Label '7z LZMA2 Fastest Preset' -Mode 'LZMA2' -Level 1),
-        (Compress-7z $Target -Label '7z LZMA2 Fast Preset' -Mode 'LZMA2' -Level 3),
-        (Compress-7z $Target -Label '7z LZMA2 Normal Preset' -Mode 'LZMA2' -Level 5),
-        (Compress-7z $Target -Label '7z LZMA2 Maximum Preset' -Mode 'LZMA2' -Level 7),
-        (Compress-7z $Target -Label '7z LZMA2 Ultra Preset' -Mode 'LZMA2' -Level 9),
-        (Compress-7z $Target -Label '7z Deflate Fastest Preset' -Mode 'Deflate' -Level 1),
-        (Compress-7z $Target -Label '7z Deflate Fast Preset' -Mode 'Deflate' -Level 3),
-        (Compress-7z $Target -Label '7z Deflate Normal Preset' -Mode 'Deflate' -Level 5),
-        (Compress-7z $Target -Label '7z Deflate Maximum Preset' -Mode 'Deflate' -Level 7),
-        (Compress-7z $Target -Label '7z Deflate Ultra Preset' -Mode 'Deflate' -Level 9),
-        (Compress-7z $Target -Label '7z Deflate64 Fastest Preset' -Mode 'Deflate64' -Level 1),
-        (Compress-7z $Target -Label '7z Deflate64 Fast Preset' -Mode 'Deflate64' -Level 3),
-        (Compress-7z $Target -Label '7z Deflate64 Normal Preset' -Mode 'Deflate64' -Level 5),
-        (Compress-7z $Target -Label '7z Deflate64 Maximum Preset' -Mode 'Deflate64' -Level 7),
-        (Compress-7z $Target -Label '7z Deflate64 Ultra Preset' -Mode 'Deflate64' -Level 9),
-        (Compress-7z $Target -Label '7z BZip2 Fastest Preset' -Mode 'BZip2' -Level 1),
-        (Compress-7z $Target -Label '7z BZip2 Fast Preset' -Mode 'BZip2' -Level 3),
-        (Compress-7z $Target -Label '7z BZip2 Normal Preset' -Mode 'BZip2' -Level 5),
-        (Compress-7z $Target -Label '7z BZip2 Maximum Preset' -Mode 'BZip2' -Level 7),
-        (Compress-7z $Target -Label '7z BZip2 Ultra Preset' -Mode 'BZip2' -Level 9),
-        (Compress-7z $Target -Label '7z BZip2 Fastest Preset' -Mode 'PPMd' -Level 1),
-        (Compress-7z $Target -Label '7z PPMd Fast Preset' -Mode 'PPMd' -Level 3),
-        (Compress-7z $Target -Label '7z PPMd Normal Preset' -Mode 'PPMd' -Level 5),
-        (Compress-7z $Target -Label '7z PPMd Maximum Preset' -Mode 'PPMd' -Level 7),
-        (Compress-7z $Target -Label '7z PPMd Ultra Preset' -Mode 'PPMd' -Level 9)
-    )
+
+    $Results = @()
+    foreach ($Mode in @('LZMA', 'LZMA2', 'Deflate', 'Deflate64', 'PPMd')) {
+        foreach ($Level in [CompressionPreset].GetEnumValues()) {
+           #$Results += Compress-7z $Target -Label "7z *.7z $Mode $($Level -As [CompressionPreset]) Preset" -Mode $Mode -Level $Level
+        }
+    }
+    foreach ($Mode in @('LZMA', 'LZMA2', 'Deflate', 'Deflate64', 'PPMd')) {
+        foreach ($Level in [CompressionPreset].GetEnumValues()) {
+            #$Results += Compress-7z $Target -Label "7z *.zip $Mode $($Level -As [CompressionPreset]) Preset" -Type 'Zip' -Mode $Mode -Level $Level
+        }
+    }
+    foreach ($Level in [CompressionPreset].GetEnumValues()) {
+        $Results += Compress-7z $Target -Label "7z *.xz $($Level -As [CompressionPreset]) Preset" -Type 'xz' -Mode 'LZMA2' -Level $Level
+    }
+    if ($Target.PSIsContainer -eq $false) {
+        # Cannot compress folders directly into gzip or bz2
+        foreach ($Type in @('gzip', 'bzip2')) {
+            foreach ($Level in [CompressionPreset].GetEnumValues()) {
+                #$Results += Compress-7z $Target -Label "7z *.$Type $($Level -As [CompressionPreset]) Preset" -Type $Type -Mode 'deflate' -Level $Level
+            }
+        }
+    }
     return $Results
-}
-
-
-Function Compress_7zLZMA {
-    Param(
-        $Target,
-        [string]$Path = (Resolve-Path '.').Path,
-        [string]$FileName = [guid]::NewGuid().Guid + '.7z',
-        [int]$Level=9
-    )
-    $Dest = $($Path + '\' + $FileName)
-    return Compress-7z $Target $Dest '7z' 'LZMA' $Level
-}
-
-Function Compress_7zDeflate {
-    Param(
-        $Target,
-        [string]$Path = (Resolve-Path '.').Path,
-        [string]$FileName = [guid]::NewGuid().Guid + '.7z',
-        [int]$Level=9
-    )
-    $Dest = $($Path + '\' + $FileName)
-    return Compress-7z $Target $Dest '7z' 'Deflate64' $Level
 }
 
 Export-ModuleMember *_*
--- Test.ps1
+++ Test.ps1
@@ -2,7 +2,7 @@
 Import-Module -Force './Compressors.psm1'
 Import-Module -Force './Analysis.psm1'
 
-$Source = Get-Item -Path '.\Corpus\'
+$Source = Get-Item -Path '.\Corpus\King James.txt'
 $Dest = Compress_7z $Source
 $Result = $Dest | %{ Analyze_Result $_ $False}
 Write-Host $Result
