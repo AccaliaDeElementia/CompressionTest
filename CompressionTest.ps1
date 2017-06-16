55f372ea0084babc8330e35861c40a42b4124fbd
accalia <accalia.de.elementia@gmail.com>
2017-06-16 08:49:06 -0400

    juggle the tests, add a git repository compression

--- CompressionTest.ps1
+++ CompressionTest.ps1
@@ -31,23 +31,32 @@ Function Test-Source {
 
 $Compressors = Get_Compressors
 
-$Sources = Get-Item -Path '.\Corpus\*.txt'
-
-foreach ($Source in $Sources) {
-    Test-Source $Source $Compressors
-}
+# Disable Individual Text File Tests. They took too long to run in CI and were not very valuable
+#$Sources = Get-Item -Path '.\Corpus\*.txt'
+#
+#foreach ($Source in $Sources) {
+#    Test-Source $Source $Compressors
+#}
 
 $Source = Get-Item -Path './Corpus'
 Test-Source $Source $Compressors
 
-Write-Host "Generating 50MB of random binary data"
-$Source = New_RandomBinaryFile "$TempDir/RandomBinary.dat" -FileSize 50mb
-Test-Source $Source $Compressors 'RandomBinary'
+Write-Host "Generating 10MB of Random Binary Data..."
+$TimeData = Measure-Command {$Source = New_RandomBinaryFile "$TempDir/RandomBinary.dat" -FileSize 10mb}
+Write-Host "Generated in $($TimeData.TotalSeconds)s"
+Test-Source $Source $Compressors
 
-Write-Host "Generating 50MB of random ASCII data"
-$Source = New_RandomTextFile "$TempDir/RandomASCII.dat" -FileSize 50mb
-Test-Source $Source $Compressors 'RandomBinary'
+Write-Host "Generating 10MB of Random ASCII Data"
+$TimeData = Measure-Command {$Source = New_RandomTextFile "$TempDir/RandomASCII.dat" -FileSize 10mb}
+Write-Host "Generated in $($TimeData.TotalSeconds)s"
+Test-Source $Source $Compressors
 
-Remove-Item -Recurse $TempDir
+Write-Host "Fetching jQuery Source"
+$Source = New-Item -ItemType Directory "$TempDir/jquery"
+$TimeData = Measure-Command {git clone https://github.com/jquery/jquery.git $Source}
+Write-Host "Source fetched in $($TimeData.TotalSeconds)s"
+Test-Source $Source $Compressors
+
+Remove-Item -Recurse -Force $TempDir
 
 Write-Host "Done"
---
+++ ResetModules.ps1
@@ -0,0 +1,10 @@
+﻿Write-Host "Resetting Powershell modules"
+Import-Module -Force './Utilities.psm1'
+Import-Module -Force './Compressors.psm1'
+Import-Module -Force './Analysis.psm1'
+
+# Create end results Directory
+$TempDir = New-Item -Type Directory Temp -Force
+Remove-Item -Recurse $TempDir
+
+Write-Host "Done"
